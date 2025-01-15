7371108d665794f0cefab7324619637521d7b0a4
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-01-15 17:02:46 -0500

    Commit latest figures (.png)

---
+++ figures/figS2-missing-patterns.R
@@ -0,0 +1,23 @@
+# Load packages 
+library(dplyr) ## for data wrangling
+library(naniar) ## for missing data plots
+
+# Load data
+## Error-Prone Data from the EHR
+summ_data = read.csv("~/Documents/Allostatic_load_audits/summary_data.csv") |> 
+  mutate(ANY_ENCOUNTERS = as.numeric(NUM_ENCOUNTERS > 0)) ### Create indicator of >= 1 healthcare encounter (ED visit or hospital admission)
+
+## Subset to only the variables needed for the analysis 
+summ_data = summ_data |> 
+  select(PAT_MRN_ID, ANY_ENCOUNTERS, AGE_AT_ENCOUNTER, SEX, 
+         CREAT_C, ALB, BMI, BP_SYSTOLIC, BP_DIASTOLIC, A1C, CHOL, TRIG, CRP, HCST)
+
+## Plot missingness
+plot = summ_data |> 
+  magrittr::set_colnames(c("PAT_MRN_ID", "ANY_ENCOUNTERS", "AGE", "SEX",
+                           "CREATININE CLEARANCE",  "SERUM ALBUMIN",
+                           "BODY MASS INDEX", "SYSTOLIC BLOOD PRESSURE",
+                           "DIASTOLIC BLOOD PRESSURE", "HEMOGLOBIN A1C", 
+                           "TOTAL CHOLESTEROL", "TRIGLYCERIDES",
+                           "C-REACTIVE PROTEIN", "HOMOCYSTEINE")) |> 
+  gg_miss_upset()
