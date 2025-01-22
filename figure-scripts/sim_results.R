cd0f333b28686050be0b8612a0f5a21470130f21
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2025-01-22 10:31:57 -0500

    Commit sim_results figure script (.R)

--- figure-scripts/sim_results.R
+++ figure-scripts/sim_results.R
@@ -164,7 +164,7 @@ ggsave(filename = "~/Dropbox (Wake Forest University)/5 - CONFERENCES/2 - Slides
        device = "png", width = 12, height = 7, units = "in")
 
 # Read in data
-sim_dir = "~/Documents/ALI_EHR/sim-data/vary_stratXstar/"
+sim_dir = "~/Documents/ALI_EHR/sim-data/vary_stratXstar_optMLE2/"
 sim_files = paste0(sim_dir, list.files(path = sim_dir, pattern = "tpr"))
 sim_res = do.call(what = dplyr::bind_rows,
                   args = lapply(X = sim_files,
@@ -203,7 +203,7 @@ sim_res |>
   theme(legend.position = "top",
         axis.title = element_text(face = "bold"),
         legend.title = element_text(face = "bold"))
-ggsave(filename = "~/Dropbox (Wake Forest University)/5 - CONFERENCES/2 - Slides/2024/ALI-EHR-ENAR-Mar2024/vary_stratXstar_tpr_fpr_line.png",
+ggsave(filename = "~/Dropbox (Wake Forest University)/5 - CONFERENCES/2 - Slides/2024/ALI-EHR-ENAR-Mar2024/vary_stratXstar_optMLE2_tpr_fpr_boxplot.png",
        device = "png", width = 12, height = 7, units = "in")
 
 sim_res = sim_res |>
@@ -226,5 +226,71 @@ sim_res |>
   theme(legend.position = "top",
         axis.title = element_text(face = "bold"),
         legend.title = element_text(face = "bold"))
-ggsave(filename = "~/Dropbox (Wake Forest University)/5 - CONFERENCES/2 - Slides/2024/ALI-EHR-ENAR-Mar2024/vary_stratXstar_tpr_fpr_line.png",
+ggsave(filename = "~/Dropbox (Wake Forest University)/5 - CONFERENCES/2 - Slides/2024/ALI-EHR-ENAR-Mar2024/vary_stratXstar_optMLE2_tpr_fpr_line.png",
+       device = "png", width = 12, height = 7, units = "in")
+
+# Read in data
+sim_dir = "~/Documents/ALI_EHR/sim-data/vary_stratXstar_optMLE/"
+sim_files = paste0(sim_dir, list.files(path = sim_dir, pattern = "tpr"))
+sim_res = do.call(what = dplyr::bind_rows,
+                  args = lapply(X = sim_files,
+                                FUN = read.csv)) |>
+  dplyr::select(sim, tpr, fpr, dplyr::ends_with("beta1")) |>
+  tidyr::gather("strat", "beta1", -c(1:3)) |>
+  dplyr::mutate(error_sett = paste0("TPR = ", round(100 * tpr), "%, FPR = ", round(100 * fpr), "%"),
+                error_sett = factor(x = error_sett,
+                                    levels = c("TPR = 99%, FPR = 1%",
+                                               "TPR = 95%, FPR = 5%",
+                                               "TPR = 80%, FPR = 20%",
+                                               "TPR = 50%, FPR = 50%")),
+                strat = factor(x = strat,
+                               levels = c("strat10_beta1",
+                                          "strat25_beta1",
+                                          "strat50_beta1",
+                                          "strat75_beta1",
+                                          "strat90_beta1" ),
+                               labels = c("10th Percentile",
+                                          "25th Percentile",
+                                          "50th Percentile",
+                                          "75th Percentile",
+                                          "90th Percentile")))
+
+sim_res |>
+  ggplot(aes(x = error_sett, y = beta1, fill = strat)) +
+  geom_boxplot() +
+  scale_fill_manual(values = slide_colors,
+                    name = "Design:") +
+  geom_hline(yintercept = beta1,
+             linetype = 2,
+             color = "black") +
+  xlab("Error Rates in Allostatic Load Index Components") +
+  ylab("Relative Efficiency to SRS") +
+  theme_minimal(base_size = 18) +
+  theme(legend.position = "top",
+        axis.title = element_text(face = "bold"),
+        legend.title = element_text(face = "bold"))
+ggsave(filename = "~/Dropbox (Wake Forest University)/5 - CONFERENCES/2 - Slides/2024/ALI-EHR-ENAR-Mar2024/vary_stratXstar_optMLE_tpr_fpr_boxplot.png",
+       device = "png", width = 12, height = 7, units = "in")
+
+sim_res = sim_res |>
+  dplyr::group_by(error_sett, strat) |>
+  dplyr::summarize(eff = 1 / var(beta1, na.rm = TRUE))
+
+# Plot results
+sim_res |>
+  ggplot(aes(x = error_sett, y = eff, color = strat, group = strat)) +
+  geom_point(size = 2) +
+  geom_line(linewidth = 1.2) +
+  scale_color_manual(values = slide_colors,
+                     name = "Design:") +
+  geom_hline(yintercept = 1,
+             linetype = 2,
+             color = "black") +
+  xlab("Error Rates in Allostatic Load Index Components") +
+  ylab("Relative Efficiency to SRS") +
+  theme_minimal(base_size = 18) +
+  theme(legend.position = "top",
+        axis.title = element_text(face = "bold"),
+        legend.title = element_text(face = "bold"))
+ggsave(filename = "~/Dropbox (Wake Forest University)/5 - CONFERENCES/2 - Slides/2024/ALI-EHR-ENAR-Mar2024/vary_stratXstar_optMLE_tpr_fpr_line.png",
        device = "png", width = 12, height = 7, units = "in")
